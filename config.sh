41a377a24ec1d731c3f928ad2178206e6f259f34
Il Harper <hi@ilharper.com>
2023-05-18 01:46:42 +0800

    chore: add version `3.1.2-12912`

--- config.sh
+++ config.sh
@@ -1,4 +1,5 @@
 BUILD_QQNT_DATA_LIST=(
   # 顺序为从新到旧，最新版在最上方
   3.1.1-11223,https://dldir1.qq.com/qqfile/qq/QQNT/2355235c/linuxqq_3.1.1-11223
+  3.1.2-12912,https://dldir1.qq.com/qqfile/qq/QQNT/80d33f88/linuxqq_3.1.2-12912
 )
