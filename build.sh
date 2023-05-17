302000708b625a0257409872b89563e056890e3f
Il Harper <hi@ilharper.com>
2023-05-18 02:20:08 +0800

    fix: fix build script

--- build.sh
+++ build.sh
@@ -54,38 +54,40 @@ do
     esac
   done
 
-  case ${BUILD_MODE} in
-    "push")
-      # Wait for manifest update
-      sleep 60
+  # https://github.com/docker/cli/issues/2396
+  #
+  # case ${BUILD_MODE} in
+  #   "push")
+  #     # Wait for manifest update
+  #     sleep 60
 
-      BUILD_MANIFEST_AMD64_DIGEST=$(docker buildx imagetools inspect --raw ilharp/qqnt:${BUILD_TAG}-linux-amd64-up${BUILD_QQNT_VERSION} | jq --raw-output '.manifests | map(select(.platform.architecture == "amd64")) | .[0].digest')
-      BUILD_MANIFEST_ARM64_DIGEST=$(docker buildx imagetools inspect --raw ilharp/qqnt:${BUILD_TAG}-linux-arm64-up${BUILD_QQNT_VERSION} | jq --raw-output '.manifests | map(select(.platform.architecture == "arm64")) | .[0].digest')
+  #     BUILD_MANIFEST_AMD64_DIGEST=$(docker buildx imagetools inspect --raw ilharp/qqnt:${BUILD_TAG}-linux-amd64-up${BUILD_QQNT_VERSION} | jq --raw-output '.manifests | map(select(.platform.architecture == "amd64")) | .[0].digest')
+  #     BUILD_MANIFEST_ARM64_DIGEST=$(docker buildx imagetools inspect --raw ilharp/qqnt:${BUILD_TAG}-linux-arm64-up${BUILD_QQNT_VERSION} | jq --raw-output '.manifests | map(select(.platform.architecture == "arm64")) | .[0].digest')
 
-      docker manifest create ghcr.io/ilharp/docker-qqnt:${BUILD_TAG}-linux-up${BUILD_QQNT_VERSION} \
-        ghcr.io/ilharp/docker-qqnt@${BUILD_MANIFEST_AMD64_DIGEST} \
-        ghcr.io/ilharp/docker-qqnt@${BUILD_MANIFEST_ARM64_DIGEST}
-      docker manifest push ghcr.io/ilharp/docker-qqnt:${BUILD_TAG}-linux-up${BUILD_QQNT_VERSION}
+  #     docker manifest create ghcr.io/ilharp/docker-qqnt:${BUILD_TAG}-linux-up${BUILD_QQNT_VERSION} \
+  #       ghcr.io/ilharp/docker-qqnt@${BUILD_MANIFEST_AMD64_DIGEST} \
+  #       ghcr.io/ilharp/docker-qqnt@${BUILD_MANIFEST_ARM64_DIGEST}
+  #     docker manifest push ghcr.io/ilharp/docker-qqnt:${BUILD_TAG}-linux-up${BUILD_QQNT_VERSION}
 
-      docker manifest create ilharp/qqnt:${BUILD_TAG}-linux-up${BUILD_QQNT_VERSION} \
-        ilharp/qqnt@${BUILD_MANIFEST_AMD64_DIGEST} \
-        ilharp/qqnt@${BUILD_MANIFEST_ARM64_DIGEST}
-      docker manifest push ilharp/qqnt:${BUILD_TAG}-linux-up${BUILD_QQNT_VERSION}
+  #     docker manifest create ilharp/qqnt:${BUILD_TAG}-linux-up${BUILD_QQNT_VERSION} \
+  #       ilharp/qqnt@${BUILD_MANIFEST_AMD64_DIGEST} \
+  #       ilharp/qqnt@${BUILD_MANIFEST_ARM64_DIGEST}
+  #     docker manifest push ilharp/qqnt:${BUILD_TAG}-linux-up${BUILD_QQNT_VERSION}
 
-      if [ "$BUILD_TAG" != "$BUILD_AUTO_TAG" -a $BUILD_QQNT_VERSION = $BUILD_QQNT_DATA_VERSION_LATEST ]
-      then
-        docker manifest create ghcr.io/ilharp/docker-qqnt:latest \
-          ghcr.io/ilharp/docker-qqnt@${BUILD_MANIFEST_AMD64_DIGEST} \
-          ghcr.io/ilharp/docker-qqnt@${BUILD_MANIFEST_ARM64_DIGEST}
-        docker manifest push ghcr.io/ilharp/docker-qqnt:latest
+  #     if [ "$BUILD_TAG" != "$BUILD_AUTO_TAG" -a $BUILD_QQNT_VERSION = $BUILD_QQNT_DATA_VERSION_LATEST ]
+  #     then
+  #       docker manifest create ghcr.io/ilharp/docker-qqnt:latest \
+  #         ghcr.io/ilharp/docker-qqnt@${BUILD_MANIFEST_AMD64_DIGEST} \
+  #         ghcr.io/ilharp/docker-qqnt@${BUILD_MANIFEST_ARM64_DIGEST}
+  #       docker manifest push ghcr.io/ilharp/docker-qqnt:latest
 
-        docker manifest create ilharp/qqnt:latest \
-          ilharp/qqnt@${BUILD_MANIFEST_AMD64_DIGEST} \
-          ilharp/qqnt@${BUILD_MANIFEST_ARM64_DIGEST}
-        docker manifest push ilharp/qqnt:latest
-      fi
-      ;;
-    *)
-      ;;
-  esac
+  #       docker manifest create ilharp/qqnt:latest \
+  #         ilharp/qqnt@${BUILD_MANIFEST_AMD64_DIGEST} \
+  #         ilharp/qqnt@${BUILD_MANIFEST_ARM64_DIGEST}
+  #       docker manifest push ilharp/qqnt:latest
+  #     fi
+  #     ;;
+  #   *)
+  #     ;;
+  # esac
 done
